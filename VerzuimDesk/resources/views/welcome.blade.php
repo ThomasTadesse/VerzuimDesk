bc63d96e258870f13ef45dd6ae78efd5b6ccede9
ThomasTadesse <162581606+ThomasTadesse@users.noreply.github.com>
2025-06-24 10:46:14 +0200

    Final commit

--- VerzuimDesk/resources/views/welcome.blade.php
+++ VerzuimDesk/resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         <div class="flex justify-between items-center">
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                {{ __('Home') }}
+                {{ __('Dashboard') }}
             </h2>
             <span class="px-3 py-1 text-xs bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-200 rounded-full">
                 {{ now()->format('d M Y') }}
