5574b78561a6dbdf5321fa802b2aa70367760f4d
ThomasTadesse <328704@student.mboutrecht.nl>
2025-06-24 09:07:25 +0200

    group seeder update

--- VerzuimDesk/database/seeders/groupSeeder.php
+++ VerzuimDesk/database/seeders/groupSeeder.php
@@ -13,5 +13,15 @@ class GroupSeeder extends Seeder
             'code' => 'IO-SD-2408B',
             'name' => 'Software Development 2024 August B'
         ]);
+        
+        Group::create([
+            'code' => 'IO-SD-2408C',
+            'name' => 'Software Development 2024 August C'
+        ]);
+        
+        Group::create([
+            'code' => 'IO-SD-2408A',
+            'name' => 'Software Development 2024 August A'
+        ]);
     }
 }
